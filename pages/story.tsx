adb6204cc0858f71dea2db27ecb541b2b9aafc0e
alifzulkifeli <alifzulkifeli@gmail.com>
2024-11-06 21:29:11 +0800

    add chat

--- pages/story.tsx
+++ pages/story.tsx
@@ -2,7 +2,7 @@ import Page from '@/components/page'
 import Section from '@/components/section'
 
 const Story = () => (
-	<Page>
+	<Page padding={6} >
 		<Section>
 			<h2 className='text-xl font-semibold'>Story</h2>
 
