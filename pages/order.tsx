3eac780235f803e9e99bc6071f36dd73a8623a94
alifzulkifeli <alifzulkifeli@gmail.com>
2024-11-06 21:26:37 +0800

    add chat

--- pages/order.tsx
+++ pages/order.tsx
@@ -2,7 +2,7 @@ import Page from '@/components/page'
 import Section from '@/components/section'
 
 const Recipes = () => (
-	<Page>
+	<Page padding={6} >
 		<Section>
             <h1>Order Page</h1>
 		</Section>
